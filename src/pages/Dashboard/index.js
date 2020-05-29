c82e6448d14bb5ef83d4678f79fa13cb6f5b2d93
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-05-29 13:35:11 -0300

    Listando agendamentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import {View} from 'react-native';
-
+import React, {useEffect, useState} from 'react';
+import api from '../../services/api';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '../../components/Background';
@@ -8,17 +7,26 @@ import Appointment from '../../components/Appointment';
 
 import {Container, Title, List} from './styles';
 
-const data = [1, 2, 3, 4, 5];
-
 export default function Dashboard() {
+  const [appointments, setAppointmens] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
+
+      setAppointmens(response.data);
+    }
+    loadAppointments();
+  }, []);
+
   return (
     <Background>
       <Container>
         <Title>Agendamentos</Title>
 
         <List
-          data={data}
-          keyExtractor={item => String(item)}
+          data={appointments}
+          keyExtractor={item => String(item.id)}
           renderItem={({item}) => <Appointment data={item} />}
         />
       </Container>
