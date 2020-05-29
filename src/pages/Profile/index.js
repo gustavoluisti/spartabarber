862aad76f76dea6ff5a396a2115095d08503ce9e
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-05-29 14:45:23 -0300

    Listando agendamentos

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,103 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {View} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '../../components/Background';
 
+import {
+  Container,
+  Title,
+  Form,
+  FormInput,
+  Separator,
+  SubmitButton,
+} from './styles';
+
 export default function Profile() {
-  return <Background />;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [oldPassword, setOldPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const oldPasswordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  function handleSubmit() {}
+
+  return (
+    <Background>
+      <Container>
+        <Title>Meu Perfil</Title>
+      </Container>
+
+      <Form>
+        <FormInput
+          icon="person-outline"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Nome completo"
+          returnKeyType="next"
+          onSubmitEditing={() => emailRef.current.focus()}
+          value={name}
+          onChangeText={setName}
+        />
+        <FormInput
+          icon="mail-outline"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Digite seu e-mail"
+          ref={emailRef}
+          returnKeyType="next"
+          onSubmitEditing={() => oldPasswordRef.current.focus()}
+          value={email}
+          onChangeText={setEmail}
+        />
+
+        <Separator />
+
+        <FormInput
+          icon="lock-outline"
+          keyboardType="email-address"
+          secureTextEntry
+          placeholder="Sua senha atual"
+          ref={oldPasswordRef}
+          returnKeyType="next"
+          onSubmitEditing={() => passwordRef.current.focus()}
+          value={oldPassword}
+          onChangeText={setOldPassword}
+        />
+
+        <FormInput
+          icon="lock-outline"
+          keyboardType="email-address"
+          secureTextEntry
+          placeholder="Sua nova senha"
+          ref={passwordRef}
+          returnKeyType="next"
+          onSubmitEditing={() => confirmPasswordRef.current.focus()}
+          value={password}
+          onChangeText={setPassword}
+        />
+
+        <FormInput
+          icon="lock-outline"
+          keyboardType="email-address"
+          secureTextEntry
+          placeholder="Confirmação de senha"
+          ref={confirmPasswordRef}
+          returnKeyType="send"
+          onSubmitEditing={handleSubmit}
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+        <SubmitButton onPress={handleSubmit}>Atualizar perfil</SubmitButton>
+      </Form>
+    </Background>
+  );
 }
 
 Profile.navigationOptions = {
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -7,12 +7,9 @@ import {updateProfileSuccess, updateProfileFailure} from './actions';
 
 export function* updateProfile({payload}) {
   try {
-    const {name, email, avatar_id, ...rest} = payload.data;
+    const {name, email, ...rest} = payload.data;
 
-    const profile = Object.assign(
-      {name, email, avatar_id},
-      rest.oldPassword ? rest : {},
-    );
+    const profile = Object.assign({name, email}, rest.oldPassword ? rest : {});
 
     const response = yield call(api.put, 'users', profile);
 
