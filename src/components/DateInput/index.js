0bd12fc4a93cac8b31412addf43c5259188f6310
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-05-29 19:43:57 -0300

    Selecionando datas

--- src/components/DateInput/index.js
+++ src/components/DateInput/index.js
@@ -2,21 +2,21 @@ import React, {useMemo} from 'react';
 import {DatePickerAndroid, Alert} from 'react-native';
 import PropTypes from 'prop-types';
 import {format} from 'date-fns';
-import en from 'date-fns/locale/en-US';
+import pt from 'date-fns/locale/pt-BR';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import {Container, DateButton, DateText} from './styles';
 
 export default function DateInput({date, onChange}) {
   const dateFormatted = useMemo(
-    () => format(date, "MMMM do',' yyyy", {locale: en}),
+    () => format(date, "MMMM do',' yyyy", {locale: pt}),
     [date],
   );
 
   async function handleOpenPicker() {
     try {
       const {action, year, month, day} = await DatePickerAndroid.open({
-        mode: 'spinner',
+        mode: 'calendar',
         date,
         minDate: new Date(),
       });
