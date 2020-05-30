6bf73fd919a8093a3e426357c46902bd27ec7ade
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-05-30 12:50:52 -0300

    ScreenView Confirm

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import {View} from 'react-native';
+import {TouchableOpacity} from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '../../../components/Background';
 
 // import {Container} from './styles';
 
 export default function Confirm() {
-  return <View />;
+  return <Background />;
 }
+
+Confirm.navigationOptions = ({navigation}) => ({
+  title: 'Confirmar agendamento',
+  headerLeft: () => (
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <Icon name="chevron-left" size={20} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
